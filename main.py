2ef03ec41d11c8ba13ca3b2219bfb666558e97bc
hyperion-institute <thanhduy290506@gmail.com>
2026-06-11 22:21:55 +0700

    Shorten experiment name to 'CSCE'

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ EPOCHS = 10
 BATCH_SIZE = 64
 LEARNING_RATE = 1e-3
 
-EXPERIMENT_NAME = "cosine_similarity_contrast_encode"
+EXPERIMENT_NAME = "CSCE"
 
 """***** Data *****"""
 transform = transforms.Compose([
